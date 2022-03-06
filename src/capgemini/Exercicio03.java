6f006494ba5772d5b44534c489a2145bfdcd577a
ONDA4PP <ondaredesocial@gmail.com>
2022-03-06 20:52:41 -0300

    versão final Exercicio03

--- src/capgemini/Exercicio03.java
+++ src/capgemini/Exercicio03.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class Exercicio03 {
 	
 	static void gridString(String s) {
+		//Calcula o tamanho da frase sem considerar espaços
 		int fraseSemEspaco = s.length();
 		int k=0, linha, coluna;
+		//Calcula tamanho da linha e coluna baseado na raiz quadrada da fraseSemEspaco
 		linha = (int) Math.floor(Math.sqrt(fraseSemEspaco));
 		coluna = (int) Math.ceil(Math.sqrt(fraseSemEspaco));
 		
+		//Garante que o tamanho da matriz será suficiente
 		if (linha * coluna < fraseSemEspaco) {
 			linha = coluna;
 		}
 		
+		//Cria a matriz e a matriz transposta
 		char grid[][] = new char[linha][coluna];
+		char[][] transposta = new char [grid[0].length][grid.length];
 		
+		//Faz o preenchimento da matriz com o uso da frase inserida pelo usuário
 		for (int l=0;l<linha;l++) {
 			for (int c=0;c<coluna;c++) {
 				if (k < s.length()) {
@@ -23,16 +29,25 @@ public class Exercicio03 {
 				}
 				k++;
 			}
+			
 		}
 		
+		//Faz a conversão da matriz (grid) para a matriz transposta (transposta)
+		for (int l=0;l<linha;l++) {
+			for (int c=0;c<coluna;c++) {
+				transposta[l][c] = grid[c][l];
+			}
+		}
+		
+		//Imprime a matriz invertida em apenas uma linha, com espaço entre as colunas
 		for(int l=0;l<linha;l++) {
 			for (int c=0;c<coluna;c++) {
 				if (grid[l][c] == 0) {
 					break;
 				}
-				System.out.print(grid[l][c]);
+				System.out.print(transposta[l][c]);
 			}
-			System.out.println("");
+			System.out.print(" ");
 		}
 	}
 
@@ -44,7 +59,7 @@ public class Exercicio03 {
 		//Leitura da frase
 		String s0 = input.nextLine();
 		String s = s0.replaceAll("\\s+", "");
-		//Executa a função que transforma String-Grid
+		//Executa a função que transforma String-Grid e imprime a matriz transposta
 		gridString(s);
 	}
 }
